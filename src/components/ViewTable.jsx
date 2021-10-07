479e59f40e49bc3900f50082d4d1987c4961834b
rafael my master <sorafaelbook@gmail.com>
2021-10-07 15:50:46 +0400

    view table

--- src/components/ViewTable.jsx
+++ src/components/ViewTable.jsx
@@ -1,28 +1,123 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Typography} from '@material-ui/core'
 import * as XLSX from 'xlsx'
 
 const ViewTable = () => {
 
 const [allData, setAllData] = useState()
+const [shetName, setShetName] = useState()
+const [choices, setChoices] = useState()
+const [headerNames, setHeaderNames] = useState()
 
-const readFile = () => {
+const [wb, setWb] = useState()
+
+const [isChoiceOpen, setIsChoiceOpen] = useState(false)
+
+const readFile = async e => {
+
+const theFile = e.target.files[0]
+if(!theFile.name.includes("xlsx")) return alert("Invalid Format")
+
+try {
+    await new Promise( (resolve, reject) => {
+        const fileReader = new FileReader()
+        fileReader.readAsArrayBuffer(theFile)
+        fileReader.onload = e => {
+        const workbook = XLSX.read(e.target.result, {type: "buffer"})
+        setWb(workbook)
+        const choiceBx = [];
+        workbook.SheetNames.forEach( wName => {
+            choiceBx.push(wName)
+        })
+        
+        setChoices(choiceBx)
+        setIsChoiceOpen(true)
+
+        }
+    })
+} catch (error) {
+    console.log(error)
+}
+
+}
+
+const processData = () => {
+
+
+const wSheet = wb.Sheets[shetName];
+const data = XLSX.utils.sheet_to_json(wSheet)
+
+setAllData(data)
 
 }
 
 const insertData = () => (
-<div className="to-center">
-    <label for="inpFile">
+
+<div className={shetName ? "nothing" : "to-center"}>
+    <label htmlFor="inpFile">
     <Typography style={{cursor: "pointer"}}
     variant="h6" gutterBottom>Insert Your Excel Data</Typography>
     </label>
-    <input id="inpFile" type="file" onChange="readFile" />
+    <input id="inpFile" type="file" onChange={readFile} />
+    
 </div>
 )
+
+useEffect( () => {
+    if(!shetName){
+        console.log("wala pang choice")
+    }else{
+        processData()
+    }
+   
+}, [shetName])
+
+useEffect( () => {
+    let hNames;
+    if(!allData){
+        console.log("no data for now")
+    }else{
+        allData.forEach( (ad, idx) => {
+            hNames = Object.keys(ad)
+        })
+       setHeaderNames(hNames)
+    }
+
+}, [allData])
 
     return (
         <div className="view-table">
             {!allData ? insertData() : <React.Fragment></React.Fragment>}
+            {!isChoiceOpen ? <React.Fragment></React.Fragment> : 
+            <div className="choice-bx">
+            
+                    {choices.map( (chs, idx) => (
+
+                    <h5 className="chs-h5" onClick={e => {
+                    setShetName(e.target.innerText)
+
+                    }} key={idx}>{chs}</h5>
+
+                    ))}
+            </div>
+            }
+            <table>
+                <thead>
+                    <tr>
+                        {!headerNames ? <React.Fragment></React.Fragment> : 
+                        headerNames.map( (hnam,idx) => <th className="vt-th" key={idx}>{hnam}</th>)
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                    {!allData ? <React.Fragment></React.Fragment> :
+                    allData.map( (adata,idx) => 
+                    <tr key={idx}>
+                       <td>{adata.CarModel}</td>
+                    </tr>)
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
